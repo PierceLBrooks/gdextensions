d5d74bfc81404a81db19c79d1b23f0231f615d84
Pierce Brooks <piercebrooks@rocketmail.com>
2022-09-24 15:07:07 -0400

    fix macos

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git submodule update --init --recursive
-python3 -m pip install -r ./requirements.txt
-python3 -m pip install -r ./Buildster/requirements.txt
+sudo python3 -m pip install -r ./requirements.txt
+sudo python3 -m pip install -r ./Buildster/requirements.txt
 python3 ./Buildster/buildster/buildster.py ./Project.xml
