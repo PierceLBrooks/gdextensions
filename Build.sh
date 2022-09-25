a9aadefc2304629fd14f4833e99f1f4771c14fd3
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2022-09-25 19:30:19 -0400

    use buildster architecture

--- Build.sh
+++ Build.sh
@@ -2,4 +2,4 @@
 git submodule update --init --recursive
 sudo python3 -m pip install -r ./requirements.txt
 sudo python3 -m pip install -r ./Buildster/requirements.txt
-python3 ./Buildster/buildster/buildster.py ./Project.xml BUILDSTER_UNAME `uname -m`
+python3 ./Buildster/buildster/buildster.py ./Project.xml
