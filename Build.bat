2d65401aa51c1f0e39562978edd5d712efd14c28
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2023-04-01 15:49:23 -0400

    a build target fix

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 git submodule update --init --recursive
 python -m pip install -r ./requirements.txt
 python -m pip install -r ./Buildster/requirements.txt
-python ./Buildster/buildster/buildster.py ./Project.xml
+python ./Buildster/buildster/buildster.py ./Project.xml BUILDSTER_VARIANT=Release
